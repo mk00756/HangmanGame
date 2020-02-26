cf168e5117d76980dc93aafac61c8530fbf2d98e
Jolux <32673529+Jolux@users.noreply.github.com>
2020-02-26 14:51:26 +0000

    Start of WordBank class

---
+++ Hangman/Hangman/WordBank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class WordBank
+    {
+    }
+
+
+
+    public static String wordGenerator()
+    {
+        List<String> wordBank = new List<string>;
+
+
+
+    }
+
+    private static void parseData()
+    {
+
+    }
+
+}
