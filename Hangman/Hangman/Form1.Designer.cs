a5450d4937112fc3fc5c22c687d7923e32fe0660
Mateusz Kulinski <mkulinski1998@gmail.com>
2020-02-26 14:53:53 +0000

    GUI Update 2
    Further updated GUI

--- Hangman/Hangman/Form1.Designer.cs
+++ Hangman/Hangman/Form1.Designer.cs
@@ -31,6 +31,13 @@
             this.wordLbl = new System.Windows.Forms.Label();
             this.wordLabelLbl = new System.Windows.Forms.Label();
             this.inputTxt = new System.Windows.Forms.TextBox();
+            this.guessBtn = new System.Windows.Forms.Button();
+            this.replayBtn = new System.Windows.Forms.Button();
+            this.guessLbl = new System.Windows.Forms.Label();
+            this.livesLabelLbl = new System.Windows.Forms.Label();
+            this.livesLbl = new System.Windows.Forms.Label();
+            this.hangmanPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPic)).BeginInit();
             this.SuspendLayout();
             // 
             // wordLbl
@@ -53,21 +60,81 @@
             // 
             // inputTxt
             // 
-            this.inputTxt.Location = new System.Drawing.Point(83, 110);
+            this.inputTxt.Location = new System.Drawing.Point(83, 185);
             this.inputTxt.Name = "inputTxt";
             this.inputTxt.Size = new System.Drawing.Size(100, 20);
             this.inputTxt.TabIndex = 2;
+            // 
+            // guessBtn
+            // 
+            this.guessBtn.Location = new System.Drawing.Point(12, 226);
+            this.guessBtn.Name = "guessBtn";
+            this.guessBtn.Size = new System.Drawing.Size(75, 23);
+            this.guessBtn.TabIndex = 3;
+            this.guessBtn.Text = "Make Guess";
+            this.guessBtn.UseVisualStyleBackColor = true;
+            // 
+            // replayBtn
+            // 
+            this.replayBtn.Location = new System.Drawing.Point(197, 226);
+            this.replayBtn.Name = "replayBtn";
+            this.replayBtn.Size = new System.Drawing.Size(75, 23);
+            this.replayBtn.TabIndex = 4;
+            this.replayBtn.Text = "Play Again";
+            this.replayBtn.UseVisualStyleBackColor = true;
+            // 
+            // guessLbl
+            // 
+            this.guessLbl.AutoSize = true;
+            this.guessLbl.Location = new System.Drawing.Point(12, 188);
+            this.guessLbl.Name = "guessLbl";
+            this.guessLbl.Size = new System.Drawing.Size(65, 13);
+            this.guessLbl.TabIndex = 5;
+            this.guessLbl.Text = "Your Guess:";
+            // 
+            // livesLabelLbl
+            // 
+            this.livesLabelLbl.AutoSize = true;
+            this.livesLabelLbl.Location = new System.Drawing.Point(218, 9);
+            this.livesLabelLbl.Name = "livesLabelLbl";
+            this.livesLabelLbl.Size = new System.Drawing.Size(35, 13);
+            this.livesLabelLbl.TabIndex = 6;
+            this.livesLabelLbl.Text = "Lives:";
+            // 
+            // livesLbl
+            // 
+            this.livesLbl.AutoSize = true;
+            this.livesLbl.Location = new System.Drawing.Point(259, 9);
+            this.livesLbl.Name = "livesLbl";
+            this.livesLbl.Size = new System.Drawing.Size(13, 13);
+            this.livesLbl.TabIndex = 7;
+            this.livesLbl.Text = "6";
+            // 
+            // hangmanPic
+            // 
+            this.hangmanPic.Location = new System.Drawing.Point(57, 48);
+            this.hangmanPic.Name = "hangmanPic";
+            this.hangmanPic.Size = new System.Drawing.Size(100, 50);
+            this.hangmanPic.TabIndex = 8;
+            this.hangmanPic.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.hangmanPic);
+            this.Controls.Add(this.livesLbl);
+            this.Controls.Add(this.livesLabelLbl);
+            this.Controls.Add(this.guessLbl);
+            this.Controls.Add(this.replayBtn);
+            this.Controls.Add(this.guessBtn);
             this.Controls.Add(this.inputTxt);
             this.Controls.Add(this.wordLabelLbl);
             this.Controls.Add(this.wordLbl);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPic)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,6 +145,12 @@
         private System.Windows.Forms.Label wordLbl;
         private System.Windows.Forms.Label wordLabelLbl;
         private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.Button guessBtn;
+        private System.Windows.Forms.Button replayBtn;
+        private System.Windows.Forms.Label guessLbl;
+        private System.Windows.Forms.Label livesLabelLbl;
+        private System.Windows.Forms.Label livesLbl;
+        private System.Windows.Forms.PictureBox hangmanPic;
     }
 }
 
