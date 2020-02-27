5ef523e5e6816e80362d6158ca188f8061f54a4d
James <tjwhittam@btinternet.com>
2020-02-27 10:39:41 +0000

    Test Update 0
    //

---
+++ Hangman/HangmanTest/Form1Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hangman;
+
+namespace HangmanTest
+{
+    [TestClass]
+    public class Form1Test
+    {
+        [TestMethod]
+        public void TestSetWord()
+        {
+            //run set word
+            //check if word to guess is not null or empty
+        }
+
+        [DataRow ("hello", "_____")]
+        [DataRow ("player", "______")]
+        [DataRow ("hello player", "_____ ______")]
+        [DataRow ("Hello Player", "_____ ______")]
+        [DataRow ("Hello,Player", "____________")]
+        [DataTestMethod]
+        public void TestInitWordOnScreen(string wordToGuess, string expResult)
+        {
+            //Hangman.Form1.SetWord();
+            //Form1 testForm = new Form1();
+            //testForm.Setword();
+        }
+
+        [DataRow ("hello", "hello", true)]
+        [DataRow("hello", "goodbye", false)]
+        [DataTestMethod]
+        public void TestWordIsComplete(string wordToGuess, string wordOnScreen, string expResult)
+        {
+            //set wordToGuse to wordToGuess
+            //set wordOnScreen to wordOnScreen
+            //run wordIsComplete
+        }
+    }
+}
---
+++ Hangman/HangmanTest/hangmanTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hangman;
+
+namespace HangmanTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
